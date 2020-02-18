2831200d832bd663540bf154bc84086e76c7daec
rodrigo.bisterco <rodrigo.bisterco@gmail.com>
2020-02-18 11:10:48 -0300

    testando projeto JavaClient MAVEN

---
+++ JavaClient/src/TestMaven.java
@@ -0,0 +1,17 @@
+
+public class TestMaven {
+
+	/*
+	Provavelmente executando normal dará o erro:
+	Error: Could not find or load main class TestMaven 
+	SELECIONA PROJETO > BOTÃO DIREITA > BUILD PROJECT
+	AGORA SIM > RUN AS > JAVA APPLICATION 
+	*/
+	
+	public static void main(String args[]) { 
+		System.out.println("PROJETO MAVEN - TESTANDO COM BUILD NO PROJETO... OK");	
+		
+	}
+
+	
+}
